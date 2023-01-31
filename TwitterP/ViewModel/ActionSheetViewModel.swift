f79869701c42301f0286818ce7a8a367fa043fe4
piszczdamian <pudzot@gmail.com>
2023-01-31 12:40:40 +0100

    Add ActionSheetViewModel

---
+++ TwitterP/ViewModel/ActionSheetViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ActionSheetViewModel.swift
+//  TwitterP
+//
+//  Created by Damian Piszcz on 31/01/2023.
+//
+
+import Foundation
+
+
+struct ActionSheetViewModel {
+    
+    private let user: User
+    
+    init(user: User) {
+        self.user = user
+    }
+}
+
+
